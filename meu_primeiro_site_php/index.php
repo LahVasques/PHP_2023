a8c2ddccedd54a8f76a40373e2532f19ca16b2d3
vasques <117761698+LahVasques@users.noreply.github.com>
2023-08-01 16:22:23 -0300

    Add files via upload

---
+++ meu_primeiro_site_php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+
+$txt = "PHP";
+echo "I love $txt!";
+
+$x = 10;
+$y = 10.5;
+
+echo "<br>" . $x + $y;
+
+
+?>
+
+</body>
+</html>
