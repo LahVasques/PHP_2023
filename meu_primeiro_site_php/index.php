88c6f8a8e94afd5d99d79a7f55c33ee9850e6c7b
vasques <laiss.hoga@aluno.ifsp.edu.br>
2023-08-01 16:45:33 -0300

    Update index.php

--- meu_primeiro_site_php/index.php
+++ meu_primeiro_site_php/index.php
@@ -11,9 +11,16 @@ echo "<br> I love $txt!";
 $x = 10;
 $y = 10.5;
 
-echo "<br>" . $x + $y;
+echo "<br>" . $x + $y "<br>"; 
 
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
 
+for($i=0; $i<3; $i++){
+    echo "<br> carro na posição " .$i. " ". $cars[$i];
+}
+
+echo "<br> Meu nome tem " . strlen("Laiss Tie Hoga Vasques")." letras";
 ?>
 
 </body>
